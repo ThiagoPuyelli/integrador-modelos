2096cec43d7fbfb605eea56848326148992dd801
ThiagoPuyelli <thiagopuyelli@gmail.com>
2025-06-23 02:33:43 -0300

    Cambio en la ip

--- main.py
+++ main.py
@@ -48,4 +48,4 @@ def imagen(imagen: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
